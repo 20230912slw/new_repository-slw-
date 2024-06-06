b8bd3bed3fbe6d1ee6c242619a37ff9ae731217c
20230912slw <743480782@qq.com>
2024-06-06 22:17:22 +0800

    接着改

--- code.c
+++ code.c
@@ -1,22 +1,6 @@
 #include<stdio.h>
 
 int mian(){
-      
-  
-
-<<<<<<< HEAD
-  
-  printf("我自横刀向天笑，去留肝胆两昆仑");
-=======
-  printf("test code1\n");     
-  printf("test code1\n");     
-  printf("test code1\n");     
-  printf("test code1\n");     
-  printf("test code1\n");     
-  printf("test code1\n");
-  printf("青山有幸埋忠骨，白铁无情铸佞臣");
-  printf("笑了，就那两下子吧");
-
->>>>>>> 5bfe34d (change upload)
+      printf("老弟还得练");
   return 0;
 }
