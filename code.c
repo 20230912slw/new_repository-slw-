2649f3163a2cd841a800d46e4cfb409145abf42a
20230912slw <743480782@qq.com>
2024-06-06 19:42:20 +0800

    second change upload

--- code.c
+++ code.c
@@ -9,6 +9,7 @@ int mian(){
   printf("test code1\n");     
   printf("test code1\n");     
   printf("test code1\n");     
-  printf("test code1\n");     
+  printf("test code1\n");
+  printf("青山有幸埋忠骨，白铁无情铸佞臣");
   return 0;
 }
