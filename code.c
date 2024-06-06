51bb0bee705891389a6d7745fb4f2142bab01f27
20230912slw <743480782@qq.com>
2024-06-06 20:16:08 +0800

    change upload

--- code.c
+++ code.c
@@ -4,7 +4,19 @@ int mian(){
       
   
 
+<<<<<<< HEAD
   
   printf("我自横刀向天笑，去留肝胆两昆仑");
+=======
+  printf("test code1\n");     
+  printf("test code1\n");     
+  printf("test code1\n");     
+  printf("test code1\n");     
+  printf("test code1\n");     
+  printf("test code1\n");
+  printf("青山有幸埋忠骨，白铁无情铸佞臣");
+  printf("笑了，就那两下子吧");
+
+>>>>>>> 5bfe34d (change upload)
   return 0;
 }
